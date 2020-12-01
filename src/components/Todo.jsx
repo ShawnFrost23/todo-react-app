1427644cbaddfbbd825a41a8ee888ca73b9f015b
Arth Patel <arthpatelf50@gmail.com>
2020-12-01 19:07:50 +0530

    edit a todo functionality added

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,19 +3,39 @@ import TodoForm from './TodoForm';
 import { RiCloseCircleLine } from 'react-icons/ri';
 import { TiEdit } from 'react-icons/ti';
 
-function Todo({ todos, completeTodo }) {
+function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
   const [edit, setEdit] = useState({
     id: null,
     value: '',
   })
+
+  const submitUpdate = value => {
+    updateTodo(edit.id, value);
+    setEdit({
+      id: null,
+      value: '',
+    })
+  }
+
+  if (edit.id) {
+    return (
+      <TodoForm edit={edit} onSubmit={submitUpdate} />
+    )
+  }
   return todos.map((todo, index) => (
     <div className={todo.isComplete ? 'todoRowComplete' : 'todoRow'} key={index}>
       <div key={todo.id} onClick={() => completeTodo(todo.id)}>
         {todo.text}
       </div>
       <div className="icons">
-        <RiCloseCircleLine />
-        <TiEdit />
+        <RiCloseCircleLine 
+          onClick={() => removeTodo(todo.id)}
+          className="deleteIcon"
+        />
+        <TiEdit 
+          onClick={() => setEdit({ id: todo.id, value: todo.text})}
+          className="editIcon"
+        />
       </div>
     </div>
   ))
