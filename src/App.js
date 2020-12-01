fab004b9dc43652d1d5cc9336e018b162bbb284c
Arth Patel <arthpatelf50@gmail.com>
2020-12-01 12:53:55 +0530

    Added todoList to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="todoApp">
       <h1>Todo App</h1>
-      <TodoForm />
+      <TodoList />
     </div>
   );
 }
