07b6b1fb88c3888139dd9e48ca44026f37016aa2
Arth Patel <arthpatelf50@gmail.com>
2020-12-01 12:48:47 +0530

    form functions added

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -2,13 +2,29 @@ import React, { useState } from 'react';
 
 function TodoForm() {
   const [input, setInput] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // props.onSubmit({
+    //   id: Math.floor(Math.random * 10000),
+    //   text: input
+    // })
+    
+    setInput('');
+  }
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  }
   return (
-    <form className="todoForm">
+    <form className="todoForm" onSubmit={handleSubmit}>
       <input 
         type="text" 
         placeholder="Add a TODO" 
         value={input} name="text" 
         className="todoInput"
+        onChange={handleChange}
       />
       <button className="todoButton">Add TODO</button>
     </form>
