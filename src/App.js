d1fa9f044036b4a18be2b05f15801713a3ad4b3f
Arth Patel <arthpatelf50@gmail.com>
2020-12-01 12:43:15 +0530

    added todoForm to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import TodoForm from './components/TodoForm';
 
 function App() {
   return (
-    <div className="App">
+    <div className="todoApp">
       <h1>Todo App</h1>
+      <TodoForm />
     </div>
   );
 }
